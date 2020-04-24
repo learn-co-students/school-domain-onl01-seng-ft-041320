234c47ac5a764de5d20c27b01acb3322ada1d30c
Mario Garza <mario.garza02@utrgv.edu>
2020-04-24 04:31:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  ROSTER={}
+  attr_reader :name,:roster
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+  def add_student(name,grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+      else
+        roster[grade]=[name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort()
+    self.roster.each{|grade,students|students.sort!}
+  end
+  
+  
+end
